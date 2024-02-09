9d98ca414bf8b22f51e00d65669c03958ffff01d
vasujain275 <vasujain275@gmail.com>
2024-02-09 23:49:48 +0530

    Added basic env support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 
 const app = express();
 
@@ -6,6 +7,6 @@ app.get("/",(req,res)=>{
     res.send("Hello World!");
 })
 
-app.listen(8000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("App is running......")
 })
