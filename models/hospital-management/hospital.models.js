07c2dd45d24f7b6b6f48a61d08d6c09550b7b772
vasujain275 <vasujain275@gmail.com>
2024-02-10 14:45:54 +0530

    Added basic Hospital Management Models

---
+++ models/hospital-management/hospital.models.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
